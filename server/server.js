bdcc622c232e4bee87a4d44de1d7c4c5525424c2
fidashn <87206839+fidashn@users.noreply.github.com>
2022-06-02 18:35:14 +0300

    deploying backend to heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
-const app = express()
+const cors = require ('cors')
 
+
+const app = express()
+const routes = require('./routes/api');
+const PORT = process.env.PORT || 8080;
+
+
+app.use(cors())
 app.get("/api", (req, res) => {
    res.json({ "users":
   ["user1",
    "user2"] })
 })
 
+if (process.env.NODE_ENV === 'production'){
+   app.use(express.static('client/build'))
+}
 
-
-app.listen(5000, ()=>{ console.log("Server started on port 5000") })
+app.listen(PORT, ()=>{ console.log(`Server started on${PORT}`) })
