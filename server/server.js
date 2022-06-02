adcc9d585383ac9d11671b536aae56dba5e2dd52
fidashn <87206839+fidashn@users.noreply.github.com>
2022-06-02 18:43:58 +0300

    fixed the connecting bugs

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const cors = require ('cors')
 
 
 const app = express()
-const routes = require('./routes/api');
-const PORT = process.env.PORT || 8080;
+// const routes = require('./routes/api');
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors())
